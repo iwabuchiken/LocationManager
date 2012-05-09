a20097d1ff06222aec8a153bc80d5d00382189af
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-09 17:52:09 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocationManager::Application.config.secret_token = '5a6f0ea2cb619bd9f669057e7ff7520e7412333ea792f01353c1fbf1ea1836cbacb4da9db43ede14254325e2c250aa444eafbb7f03c378f8aced0dab6b423472'
